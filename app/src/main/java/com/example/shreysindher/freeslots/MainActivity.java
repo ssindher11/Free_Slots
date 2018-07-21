8cc33fb435cc495a3466c3bad51dfde33a623e64
Piyush <piyush.kumardav@gmail.com>
2018-07-21 19:57:24 +0530

    Add a webview to display the official site

--- app/src/main/java/com/example/shreysindher/freeslots/MainActivity.java
+++ app/src/main/java/com/example/shreysindher/freeslots/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.shreysindher.freeslots;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
+    private WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        webView=(WebView) findViewById(R.id.webviewer);
+        webView.setWebViewClient(new WebViewClient());
+        // string url which you have to load into a web view
+        String url = "https://freeslot.acmvit.in/#/";
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(url); // load the url on the web view
     }
 }
